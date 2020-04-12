32233834db570737d1673ade633a744e33d7216f
Tobias Hall <44125169+tobymaxim@users.noreply.github.com>
2020-04-12 11:57:03 +0200

    adjusted artistspage

--- src/pages/artists2.js
+++ src/pages/artists2.js
@@ -4,53 +4,84 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "../styles/index.css"
 import Sociallinks from "../components/Sociallinks"
+import Nextdates from "../components/Nextdates"
 
-const IndexPage = ({ data }) => (
+const ArtistPage = ({ data }) => (
   
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <div class="main-container">
         <div class="left-container">
             <div class="blackbox"><h1 class="headline1">Artists</h1></div>
-            <div style={{ listStyle: "none" }}>{data.allWordpressPage.edges.map(post =>
-                <img className="blog-images" src={post.node.featured_media.localFile.childImageSharp.resolutions.src} alt={post.node.featured_media.alt_text} />
+            <div class="blog-post">
+            <div className="artists-images-container">{data.allWordpressPage.edges.map(post =>
+                <img className="artists-images" src={post.node.featured_media.localFile.childImageSharp.resolutions.src} alt={post.node.featured_media.alt_text} />
             )}
+            </div>
             </div>
         </div>
         <div class="right-container">
             <div class="blackbox">
-                <h1 class="headline1">Next Dates</h1>
+              <h1 class="headline1">Next Dates</h1>
             </div>
+            <Nextdates data={data} />
             <Sociallinks data={data} />
-            <div class="blackbox">
-            <h1 class="headline1">Instagram</h1>
-            </div>
-            <div className="instagram-container">asdasd</div>
         </div>
       </div>
     </Layout>
   )
   
-export default IndexPage
+export default ArtistPage
 
 
 export const query = graphql`
   query {
     allWordpressPage(filter: {template: {eq: "tpl-artists.php"}}) {
-        edges {
-          node {
-            featured_media {
-              localFile {
-                childImageSharp {
-                  resolutions(height: 530, width: 530) {
-                    src
-                  }
+      edges {
+        node {
+          featured_media {
+            localFile {
+              childImageSharp {
+                resolutions(height: 530, width: 530) {
+                  src
                 }
               }
             }
-            title
+          }
+          title
+        }
+      }
+    }
+    allTribeEvents {
+      edges {
+        node {
+          title
+          categories {
+            name
+          }
+          website
+          venue {
+            venue
+            website
+            city
+            country
+          }
+          start_date(formatString: "D. MMMM YYYY")
+        }
+      }
+    }
+    allInstaNode {
+      edges {
+        node {
+          localFile {
+            childImageSharp {
+              resolutions(height: 580, width: 580, fit: COVER, cropFocus: CENTER) {
+                src
+              }
+            }
           }
         }
       }
-    }    
+    }
+  }    
 `
