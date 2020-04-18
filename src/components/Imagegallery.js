2988a45c1d90e5e6d8a46783805ecdf0b7d1e8e2
Tobias Hall <44125169+tobymaxim@users.noreply.github.com>
2020-04-18 02:24:30 +0200

    FINALIZED artists

--- src/components/Imagegallery.js
+++ src/components/Imagegallery.js
@@ -5,7 +5,7 @@ import "../styles/imagegallery.css";
 const Imagegallery = ({ data }) => (
   <div>
     <div className="blackbox-imagegallery">
-      <h1 className="headline1">Presspics</h1>
+      <h1 className="headline1-artist">asdPresspics</h1>
     </div>
 
     <ul className="imagegallery-ul" style={{ listStyle: "none" }}>{data.allWordpressPage.edges.map(post =>
