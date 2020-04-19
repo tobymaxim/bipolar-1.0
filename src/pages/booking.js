fef035e219ca52a0416c9b9e6c6879078684d9fe
Tobias Hall <44125169+tobymaxim@users.noreply.github.com>
2020-04-19 23:25:03 +0200

    added contact form

---
+++ src/pages/booking.js
@@ -0,0 +1,88 @@
+import React from "react"
+import { graphql, } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import "../styles/index.css"
+import Sociallinks from "../components/Sociallinks"
+import Nextdates from "../components/Nextdates"
+import "./iframe"
+
+
+const BookingPage = ({ data }) => (
+
+    <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <div className="main-container">
+            <div className="left-container">
+                <div className="blackbox"><h1 className="headline1">Booking Request</h1></div>
+                <div className="blog-post">
+                    <div class="form-iframe-container">
+                    <iframe src="http://54499171.swh.strato-hosting.eu/bipolarjs/contact-form/" name="iframe" id="iframe" scrolling="no" frameborder="0" marginheight="0px" marginwidth="0px" height="422" width="100%" onload="setIframeHeight();"></iframe>
+                    </div>
+                </div>
+            </div>
+            <div className="right-container">
+                <div className="blackbox">
+                    <h1 className="headline1">Next Dates</h1>
+                </div>
+                <Nextdates data={data} />
+                <Sociallinks data={data} />
+            </div>
+        </div>
+    </Layout>
+)
+
+export default BookingPage
+
+
+export const query = graphql`
+  query {
+    allWordpressPage(filter: {template: {eq: "tpl-artists.php"}}, sort: {fields: guid}) {
+      edges {
+        node {
+          featured_media {
+            localFile {
+              childImageSharp {
+                resolutions(height: 530, width: 530) {
+                  src
+                }
+              }
+            }
+          }
+          title
+        }
+      }
+    }
+    allTribeEvents {
+      edges {
+        node {
+          title
+          categories {
+            name
+          }
+          website
+          venue {
+            venue
+            website
+            city
+            country
+          }
+          start_date(formatString: "D. MMMM YYYY")
+        }
+      }
+    }
+    allInstaNode {
+      edges {
+        node {
+          localFile {
+            childImageSharp {
+              resolutions(height: 580, width: 580, fit: COVER, cropFocus: CENTER) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  }    
+`
